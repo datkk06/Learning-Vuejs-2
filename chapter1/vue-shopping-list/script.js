f28fda96030c092fbc11c624a300650270e85c1a
datpt <thanhdatkma96@gmail.com>
2019-07-20 17:01:39 +0700

    chapter1

--- chapter1/vue-shopping-list/script.js
+++ chapter1/vue-shopping-list/script.js
@@ -1,21 +1,54 @@
-var data = {
-  items: [{ text: 'Bananas', checked: true }, { text: 'Apples', checked: false }],
-  title: 'My Shopping List',
+// var data = {
+//   items: [{ text: 'Bananas', checked: true }, { text: 'Apples', checked: false }],
+//   title: 'My Shopping List',
+//   newItem: ''
+// };
+
+var server_data = {
+  items: [{ id: '1', name: 'SVR1', status: 'ACTIVE' }],
+  title: 'Server Management',
   newItem: ''
-};
+}
+
+// new Vue({
+//   el: '#app',
+//   data: server_data,
+//   methods: {
+//     addItem: function () {
+//       var text;
+
+//       text = this.newItem.trim();
+//       if (text) {
+//         this.items.push({
+//           text: text,
+//           checked: false
+//         });
+//         this.newItem = '';
+//       }
+//     },
+//     say: function (message) {
+//       alert(message)
+//     }
+//   }
+// });
 
 new Vue({
   el: '#app',
-  data: data,
+  data: server_data,
   methods: {
     addItem: function () {
-      var text;
+      var id;
+      var name;
+      var status;
 
-      text = this.newItem.trim();
-      if (text) {
+      id = this.id.trim();
+      name = this.name.trim();
+      status = this.status.trim();
+      if (id, status, name) {
         this.items.push({
-          text: text,
-          checked: false
+          id: id,
+          name: name,
+          status: status
         });
         this.newItem = '';
       }
